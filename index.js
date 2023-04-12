0adc6f2c4f03aa1363ff646f7d738ed296fb1844
shaundcosta246 <shaundcosta246@gmail.com>
2023-04-12 01:13:06 +0100

    second update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello shaheer how are you hope you are fine");
+const menuNav = document.getElementById("menu-nav");
+const sideNavBar = document.getElementById("side-nav-bar");
+let count = 1;
+menuNav.addEventListener("click", () => {
+    if(count === 1){
+        sideNavBar.style.display ="flex";
+        sideNavBar.style.transform="translateX(0px)";
+        count++;
+    }else{
+        sideNavBar.style.transform="translateX(1000px)";
+        sideNavBar.style.display ="none";
+        count--;
+    }
+})
