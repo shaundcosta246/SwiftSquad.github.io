0b1a8465a57047017430e8dfad2abeba5d590347
shaundcosta246 <shaundcosta246@gmail.com>
2024-01-24 02:37:43 +0000

    changein db hope hoppers

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
-const localdatabase = "mongodb://127.0.0.1:27017/hoppers"
-mongoose.connect(localdatabase, {
-    useNewUrlParser: true,
-})
-.then(() => {
-    console.log("database connected")
-})
-.catch((error) => {
-    console.log(error)
-})
+// const mongoose = require("mongoose");
+// const localdatabase = "mongodb://127.0.0.1:27017/hoppers"
+// mongoose.connect(localdatabase, {
+//     useNewUrlParser: true,
+// })
+// .then(() => {
+//     console.log("database connected")
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
