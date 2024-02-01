a6fdefb0e78699b1003d65f977fdbca107ddff18
shaundcosta246 <shaundcosta246@gmail.com>
2024-02-01 18:01:09 +0000

    db change

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const conUri = `mongodb+srv://shaundcosta246:jmFHGG9Y6EPFp7ah@cluster0.vpeqywk.mongodb.net/hoppers?retryWrites=true&w=majority`
+const conUri = `mongodb+srv://shaundcosta246:jmFHGG9Y6EPFp7ah@cluster0.vpeqywk.mongodb.net/hoppersTesting?retryWrites=true&w=majority`
 const localdatabase = "mongodb://127.0.0.1:27017/hoppersTesting"
 mongoose.connect(conUri, {
     useNewUrlParser: true,
