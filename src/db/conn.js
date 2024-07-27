7bc9cf4c8bf2610a7dda762fcc950bbd109441dc
shaundcosta246 <shaundcosta246@gmail.com>
2024-07-27 01:38:55 +0100

    db update

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const localdatabase = "mongodb://127.0.0.1:27017/Hoppers"
 mongoose.connect(localdatabase, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000 // 30 seconds
 })
 .then(() => {
     console.log(console.log("database connected"));
