30445095e40c9b5b0e74c02eb6b7adb039ec9e18
shaundcosta246 <shaundcosta246@gmail.com>
2024-07-27 01:47:03 +0100

    uri update

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-const localdatabase = "mongodb://127.0.0.1:27017/Hoppers"
-mongoose.connect(localdatabase, {
+const localdatabase = "mongodb://127.0.0.1:27017/Hoppers";
+const conUri = `mongodb+srv://shaundcosta246:jmFHGG9Y6EPFp7ah@cluster0.vpeqywk.mongodb.net/hoppers?retryWrites=true&w=majority`
+mongoose.connect(conUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 30000 // 30 seconds
